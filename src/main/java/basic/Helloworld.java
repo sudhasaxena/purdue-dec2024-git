20a81bbdea79de04aa1cee320ddfd04e68b38639
Sudha S <sudhasaxena88@gmail.com>
2024-12-29 21:48:24 -0600

    Created the file from scratch

---
+++ src/main/java/basic/Helloworld.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello there, how are you?");
+
+	}
+
+}
---
+++ src/main/java/basic/Turbo.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Turbo {
+
+	public static void main(String[] args) {
+		System.out.println("Turbo is my Dog! ");
+
+	}
+
+}
